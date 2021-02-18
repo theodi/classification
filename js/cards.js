154f213563aa62b033415acabf9d0194e13c87f4
David Tarrant <davetaz@theodi.org>
2021-02-18 18:38:06 +0000

    First stab at the animated auto sort, works on the training set. Need to add input validation, ability to return cards, counting, evaluation set and possibly a test set

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,9 @@
+var cards = [];
+cards.trainingSet = [];
+cards.evaluationSet = [];
+
 function processInput() {
     val = $('#number').val();
-    console.log(val);
     if (redCount >= maxRedCount) {
         alert("You have reached your limit of " + maxRedCount + " cards!");
     }
@@ -16,6 +19,8 @@ function getRedCard(card) {
     d3.csv('data/red_cards.csv', function(data) {
         if (index == card && redCount < maxRedCount) {
             data.city = "?";
+            data.index = index;
+            cards.evaluationSet.push(data);
             renderRedCard(data,index,'red');
             redCount += 1;
             redDone[index] = true;
@@ -37,7 +42,9 @@ function getBlueCards(group,card) {
     var count = 1;
     var index = 1;
     d3.csv('data/houses.csv', function(data) {
+        data.index = index;
     	if (data.group == group) {
+            cards.trainingSet.push(data);
     		renderCard(data,index,'blue');
     		count += 1;
     	}
@@ -71,6 +78,8 @@ function getRedCards(random) {
     d3.csv('data/red_cards.csv', function(data) {
         if (random[index]) {
             data.city = "?";
+            data.index = index;
+            cards.evaluationSet.push(data);
             renderRedCardNormal(data,index,'red');
         }
         index += 1;
@@ -84,7 +93,7 @@ function removeCard(id){
 }
 
 function renderCard(data,count,color) {
-	$('#training-set').append('<card class="draggable drag-drop"><h1 class="target">'+data.city+'</h1><h1 class="number">#'+count+'</h1><image src="img/house.png"></image><table class="'+color+'"><tr><td class="attribute">Bathrooms</td><td class="value">'+data.bath+'</td></tr><tr><td class="attribute">Bedrooms</td><td class="value">'+data.beds+'</td></tr><tr><td class="attribute">Year built</td><td class="value">'+data.year_built+'</td></tr><tr><td class="attribute">Elevation</td><td class="value">'+formatNumber(data.elevation)+'ft</td></tr><tr><td class="attribute">Square Footage</td><td class="value">'+formatNumber(data.sqft)+'</td></tr><tr><td class="attribute">Price</td><td class="value">$'+formatNumber(data.price)+'</td></tr><tr><td class="attribute">Price per sqft</td><td class="value">$'+formatNumber(data.price_per_sqft)+'</td></tr></table></card>');
+	$('#training-set').append('<card class="draggable drag-drop" id="b'+count+'"><h1 class="target">'+data.city+'</h1><h1 class="number">#'+count+'</h1><image src="img/house.png"></image><table class="'+color+'"><tr><td class="attribute">Bathrooms</td><td class="value">'+data.bath+'</td></tr><tr><td class="attribute">Bedrooms</td><td class="value">'+data.beds+'</td></tr><tr><td class="attribute">Year built</td><td class="value">'+data.year_built+'</td></tr><tr><td class="attribute">Elevation</td><td class="value">'+formatNumber(data.elevation)+'ft</td></tr><tr><td class="attribute">Square Footage</td><td class="value">'+formatNumber(data.sqft)+'</td></tr><tr><td class="attribute">Price</td><td class="value">$'+formatNumber(data.price)+'</td></tr><tr><td class="attribute">Price per sqft</td><td class="value">$'+formatNumber(data.price_per_sqft)+'</td></tr></table></card>');
 }
 
 function renderRedCardNormal(data,count,color) {
@@ -92,7 +101,7 @@ function renderRedCardNormal(data,count,color) {
 }
 
 function renderRedCard(data,count,color) {
-    $('<card id="red-'+count+'" class="draggable drag-drop"><h1 class="target">'+data.city+'</h1><h1 class="number">#'+count+'</h1><image src="img/house.png"></image><table class="'+color+'"><tr><td class="attribute">Bathrooms</td><td class="value">'+data.bath+'</td></tr><tr><td class="attribute">Bedrooms</td><td class="value">'+data.beds+'</td></tr><tr><td class="attribute">Year built</td><td class="value">'+data.year_built+'</td></tr><tr><td class="attribute">Elevation</td><td class="value">'+formatNumber(data.elevation)+'ft</td></tr><tr><td class="attribute">Square Footage</td><td class="value">'+formatNumber(data.sqft)+'</td></tr><tr><td class="attribute">Price</td><td class="value">$'+formatNumber(data.price)+'</td></tr><tr><td class="attribute">Price per sqft</td><td class="value">$'+formatNumber(data.price_per_sqft)+'</td></tr></table><div></card>').insertBefore("#evaluation-button");;
+    $('<card id="red-'+count+'" class="draggable drag-drop"><h1 class="target">'+data.city+'</h1><h1 class="number">#'+count+'</h1><image src="img/house.png"></image><table class="'+color+'"><tr><td class="attribute">Bathrooms</td><td class="value">'+data.bath+'</td></tr><tr><td class="attribute">Bedrooms</td><td class="value">'+data.beds+'</td></tr><tr><td class="attribute">Year built</td><td class="value">'+data.year_built+'</td></tr><tr><td class="attribute">Elevation</td><td class="value">'+formatNumber(data.elevation)+'ft</td></tr><tr><td class="attribute">Square Footage</td><td class="value">'+formatNumber(data.sqft)+'</td></tr><tr><td class="attribute">Price</td><td class="value">$'+formatNumber(data.price)+'</td></tr><tr><td class="attribute">Price per sqft</td><td class="value">$'+formatNumber(data.price_per_sqft)+'</td></tr></table><div></card>').insertBefore("#evaluation-button");
 }
 
 
@@ -141,15 +150,87 @@ function viewInstructions(){
 }
 
 function onchange() {
-    var condition1a = $('#condition1a');
-    var condition1b = $('#condition1b');
-    condition1b.val(condition1a.val());
+    var condition_a = $('#condition_a');
+    var condition_b = $('#condition_b');
+    condition_b.val(condition_a.val());
 
-    var condition2aa = $('#condition2aa');
-    var condition2ab = $('#condition2ab');
-    condition2ab.val(condition2aa.val());
+    var condition_la = $('#condition_la');
+    var condition_lb = $('#condition_lb');
+    condition_lb.val(condition_la.val());
 
-    var condition2ba = $('#condition2ba');
-    var condition2bb = $('#condition2bb');
-    condition2bb.val(condition2ba.val());
+    var condition_ra = $('#condition_ra');
+    var condition_rb = $('#condition_rb');
+    condition_rb.val(condition_ra.val());
+}
+
+function moveAnimate(element, newParent){
+    //Allow passing in either a jQuery object or selector
+    element = $(element);
+    newParent= $(newParent);
+
+    var oldOffset = element.offset();
+    element.appendTo(newParent);
+    var newOffset = element.offset();
+
+    var temp = element.clone().appendTo('body');
+    temp.css({
+        'position': 'absolute',
+        'left': oldOffset.left,
+        'top': oldOffset.top,
+        'z-index': 1000
+    });
+    element.hide();
+    temp.animate({'top': newOffset.top, 'left': newOffset.left}, 'slow', function(){
+       element.show();
+       temp.remove();
+    });
+}
+
+function animateCard(card) {
+    moveAnimate('#b'+card.index,'#box_' + card.box);
+}
+
+function sortTrainingSet() {
+    sortCards(cards.trainingSet,"");
+}
+
+function sortCards(cardset,currentPosition) {
+    var interval = 0;
+    cardset.forEach(function(card,value,index) {
+        setTimeout(function() { 
+            animateCard(sortCard(card,currentPosition));
+        },interval);
+        interval += 800;
+    });
+}
+
+function sortCard(card,currentPosition) {
+    if (currentPosition == "") {
+        card.box = "";  
+    }
+    var factor = $('#factor_'+currentPosition).val();
+    var boundary = $('#condition_'+currentPosition+'a').val();
+    console.log(factor);
+    console.log(boundary);
+    if (!factor || !boundary) {
+        return card;
+    }
+    console.log(card[factor]);
+    if(parseFloat(card[factor]) <= boundary) {
+        currentPosition = currentPosition + "l";
+        card.box = card.box + "l";
+        if (currentPosition.length < 2) {
+            return sortCard(card,currentPosition);
+        } else {
+            return card;
+        }
+    } else {
+        currentPosition = currentPosition + "r";
+        card.box = card.box + "r";
+        if (currentPosition.length < 2) {
+            return sortCard(card,currentPosition);
+        } else {
+            return card;
+        }
+    }
 }
