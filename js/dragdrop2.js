16a68ff386b58f366d37d07a16c94afec40c659d
Jack Schofield <jackschofield1@googlemail.com>
2022-01-19 13:53:23 +0000

    updates

---
+++ js/dragdrop2.js
@@ -0,0 +1,180 @@
+var startpos = [];
+var pos = null;
+
+var drops = [];
+
+let indexes = [];
+         
+
+(function () {
+  function init() {
+    interact(".draggable").draggable({
+      snap: {
+        targets: [pos],
+        range: Infinity,
+        relativePoints: [{ x: 0.5, y: 0.5 }],
+        endOnly: true,
+      },
+      onstart: function (event) {
+        var rect = interact.getElementRect(event.target);
+
+        // record center point when starting the very first a drag
+        pos = {
+          x: rect.left + rect.width / 2,
+          y: rect.top + rect.height / 2,
+        };
+
+        if (startpos[event.target.id] == undefined) {
+          startpos[event.target.id] = pos;
+        }
+        event.interactable.draggable({
+          snap: {
+            targets: [pos],
+          },
+        });
+      },
+      // call this function on every dragmove event
+      onmove: function (event) {
+        var target = event.target,
+          // keep the dragged position in the data-x/data-y attributes
+          x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform = target.style.transform =
+          "translate(" + x + "px, " + y + "px)";
+
+        // update the posiion attributes
+        target.setAttribute("data-x", x);
+        target.setAttribute("data-y", y);
+        target.classList.add("getting--dragged");
+      },
+      onend: function (event) {
+        event.target.classList.remove("getting--dragged");
+      },
+    });
+
+    interact(".dropzone").dropzone({
+      accept: ".draggable",
+      overlap: 0.5,
+
+      ondropactivate: function (event) {
+        event.target.classList.add("can--drop");
+      },
+      ondragenter: function (event) {
+        console.log("eed", event.relatedTarget.id);
+        var draggableElement = event.relatedTarget;
+        dropzoneElement = event.target;
+        dropRect = interact.getElementRect(dropzoneElement);
+        dropCenter = {
+          x: dropRect.left + dropRect.width / 2,
+          y: dropRect.top + dropRect.height / 2,
+        };
+
+        dragableH = $(".draggable").height();
+        dragableW = $(".draggable").width();
+
+        // console.log('dze',dropzoneElement.id);
+
+        let index = 0;
+        indexes = [];
+        console.log(drops[dropzoneElement.id]);
+        if (drops[dropzoneElement.id] != undefined) {
+          for (var i = 0; i < 10; i++) {
+            indexes[i] = 1;
+          }
+          for (let [key, value] of Object.entries(drops[dropzoneElement.id])) {
+            indexes[value] = 0;
+          }
+          
+          for (var i = 0; i < 10; i++) {
+            if( indexes[i] == 1){
+              index = i;
+              break;
+            }
+          }
+          console.log("index", index);
+        } else {
+          let index = 0;
+        }
+        
+        // console.info("index", index);
+
+        dropCenter = {
+          x: dropRect.left + dragableW / 2 + index * dragableW,
+          y: dropRect.top + dragableH / 2,
+        };
+
+        // console.log(dropCenter);
+
+        event.draggable.draggable({
+          snap: {
+            targets: [dropCenter],
+          },
+        });
+
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add("can--catch");
+        draggableElement.classList.add("drop--me");
+      },
+      ondragleave: function (event) {
+        if (drops[event.target.id][event.relatedTarget.id] != undefined) {
+          delete (drops[event.target.id][event.relatedTarget.id]);
+        }
+        console.log("dragleavc", event);
+        // remove the drop feedback style
+        // event.target.classList.remove("can--catch", "caught--it");
+        // event.relatedTarget.classList.remove("drop--me");
+        // // console.log(event.relatedTarget.id);
+        event.draggable.draggable({
+          snap: {
+            targets: [startpos[event.relatedTarget.id]],
+          },
+        });
+      },
+      ondrop: function (event) {
+        if (drops[event.target.id] == undefined) {
+          drops[event.target.id] = [];
+        }
+        drops[event.target.id][event.relatedTarget.id] = Object.keys(drops[event.target.id]).length;
+        console.log("Index of dropped node: " + event.target);
+        console.log(
+          "Index of dragged node: " +
+            getNodeIndex(event.relatedTarget.parentNode)
+        );
+        // event.relatedTarget.textContent = 'Dropped';
+        // console.log("Dropped!");
+        console.log("related target: " + event.relatedTarget.parentNode);
+        // console.log(event.draggable);
+        event.target.classList.add("caught--it");
+      },
+      ondropdeactivate: function (event) {
+        // remove active dropzone feedback
+        event.target.classList.remove("can--drop");
+        event.target.classList.remove("can--catch");
+      },
+    });
+  }
+
+  function getNodeIndex(node) {
+    var index = 0;
+    while ((node = node.previousSibling)) {
+      if (node.nodeType != 3 || !/^\s*$/.test(node.data)) {
+        index++;
+      }
+    }
+    return index;
+  }
+
+  function eleHasClass(el, cls) {
+    return (
+      el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className)
+    );
+  }
+
+  window.onload = function () {
+    init();
+  };
+
+  function getsnappos(target) {}
+})();
